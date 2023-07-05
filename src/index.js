23ad8c0273aa7476e9255423726d4f786410c4f9
TaeWon Park <thisis_tw94@naver.com>
2023-07-05 20:41:25 +0900

    router 셋팅

--- src/index.js
+++ src/index.js
@@ -2,6 +2,37 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter } from "react-router-dom";
+import NotFound from "./pages/NotFound";
+import Home from "./pages/Home";
+import AllProducts from "./pages/AllProducts";
+import ProductDetail from "./pages/ProductDetail";
+import MyCart from "./pages/MyCart";
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <NotFound />,
+    children: [
+      { index: true, path: '/', element: <Home />, },
+      {
+        path: '/products',
+        element: <AllProducts />,
+      },
+      {
+        path: '/products/:id',
+        element: <ProductDetail />,
+      },
+      {
+        path: '/cart',
+        element: <MyCart />,
+      },
+    ]
+  }
+])
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
