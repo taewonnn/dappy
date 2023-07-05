0f29479413ce068601549b40172deb5e67486769
TaeWon Park <thisis_tw94@naver.com>
2023-07-05 20:47:02 +0900

    NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,22 @@
+import { BsFillPencilFill } from "react-icons/bs";
+import { Link } from "react-router-dom";
+import { FiShoppingBag } from "react-icons/fi";
+
 export default function NavBar() {
   return (
-    <>
-      NavBar
-    </>
+    <header>
+      <Link to='/'>
+        <FiShoppingBag />
+        <h1>Dappy</h1>
+      </Link>
+      <nav>
+        <Link to='/products'>Products</Link>
+        <Link to='/carts'>Carts</Link>
+        <Link to='/products/new'>
+          <BsFillPencilFill />
+        </Link>
+        <button>Login</button>
+      </nav>
+    </header>
   )
 }
