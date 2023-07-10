062897e47ac917bc735056824018c5fb43e546a0
TaeWon Park <thisis_tw94@naver.com>
2023-07-10 20:54:35 +0900

    User 정보

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import { FiShoppingBag } from "react-icons/fi";
 import { login, logout, onUserStateChange } from "../api/firebase";
 import { useEffect, useState } from "react";
+import User from "./User";
 
 export default function NavBar() {
 
@@ -16,13 +17,13 @@ export default function NavBar() {
     })
   }, [])
 
-  const handleLogin = () => {
-    login();
-  }
+  // const handleLogin = () => {
+  //   login();
+  // }
 
-  const handleLogout = () => {
-    logout();
-  }
+  // const handleLogout = () => {
+  //   logout();
+  // }
 
   return (
     <header className='flex justify-between border-b border-gray-300 p-2'>
@@ -36,8 +37,9 @@ export default function NavBar() {
         <Link to='/products/new' className='text-2xl '>
           <BsFillPencilFill />
         </Link>
-        { !user && <button onClick={handleLogin}>Login</button> }
-        { user && <button onClick={handleLogout}>Logout</button> }
+        { user && <User user={user}/>}
+        { !user && <button onClick={login}>Login</button> }
+        { user && <button onClick={logout}>Logout</button> }
       </nav>
     </header>
   )
