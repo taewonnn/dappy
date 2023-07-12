dd1c6f231a838f1c79354a1722c6bca028944b1e
TaeWon Park <thisis_tw94@naver.com>
2023-07-12 21:55:27 +0900

    protectedRoute

--- src/pages/ProtectedRoute.jsx
+++ src/pages/ProtectedRoute.jsx
@@ -3,10 +3,11 @@ export default function ProtectedRoute({ children, requireAdmin }) {
   // 그 사용자가 어드민 권한이 있는지 확인
   // requireAdmin이 true인 경우에는 로그인도 되고, 어드민 권한도 가지고 있어야 한다.
   // 조건에 맞지 않다면 / 상위 경로로 이동시키기
+  // 조건에 맞는 경우에만 전달된 children을 보여주기
 
   return (
     <>
-
+      protect
     </>
 
   )
