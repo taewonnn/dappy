9ed3781217a992136b773e6fdc4c0833c17f4aab
TaeWon Park <thisis_tw94@naver.com>
2023-07-12 20:49:47 +0900

    button 스타일링

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,6 +1,6 @@
 export default function ({user: {photoURL, displayName}}) {
   return (
-    <div className='flex items-center'>
+    <div className='flex items-center shrink-0'>
       <img className='w-10 h-10 rounded-full mr-2' src={photoURL} alt={displayName}/>
       <span className='hidden md:block '>{displayName}</span>
     </div>
