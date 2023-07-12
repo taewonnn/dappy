59bfb2c055e6572a8fceed63bd255017d8ee3bbf
TaeWon Park <thisis_tw94@naver.com>
2023-07-12 21:53:15 +0900

    context - AuthContext(로그인 여부 확인)

--- src/components/context/AuthContext.jsx
+++ src/components/context/AuthContext.jsx
@@ -1,8 +1,17 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
+import { onUserStateChange } from "../../api/firebase";
 
 const AuthContext = createContext();
 
 export function AuthContextProvider({ children }) {
+  const [user, setUser] = useState();
+
+  useEffect(() => {
+    onUserStateChange((user) => {
+      console.log(user);
+      setUser(user);
+    })
+  }, [])
 
   return (
     <AuthContext.Provider>
