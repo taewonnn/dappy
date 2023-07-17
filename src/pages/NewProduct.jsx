7357ea42412870582f982fb2c468a15d9647f581
TaeWon Park <thisis_tw94@naver.com>
2023-07-17 20:26:20 +0900

    new product - img

--- src/pages/NewProduct.jsx
+++ src/pages/NewProduct.jsx
@@ -2,12 +2,14 @@ import { useState } from "react";
 
 export default function NewProduct() {
   const [product, setProduct] = useState({});
+  const [file, setFile] = useState();
 
-  const handleChange = () => {
-
+  const handleChange = (e) => {
+    const { name, value, files } = e.target;
+    setProduct((product) => ({...product, [name] : value}))
   }
 
-  const handleSubmit = () => {
+  const handleSubmit = (e) => {
 
   }
 
