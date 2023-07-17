0a5e44573863ee9c90768cf3561545992aa11bd9
TaeWon Park <thisis_tw94@naver.com>
2023-07-17 20:27:23 +0900

    new product - file

--- src/pages/NewProduct.jsx
+++ src/pages/NewProduct.jsx
@@ -6,6 +6,10 @@ export default function NewProduct() {
 
   const handleChange = (e) => {
     const { name, value, files } = e.target;
+    if(name === 'file') {
+      setFile(files && files[0]);
+      return; // return을 쓰는 이유 : 파일이 저장되었다면 밑에 setProduct는 필요없어서 여기서 끝내기 위해
+    }
     setProduct((product) => ({...product, [name] : value}))
   }
 
