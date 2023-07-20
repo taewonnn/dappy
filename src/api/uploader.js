c3da50c5fc13f153e1ac8a3e922eff128a3c80e7
TaeWon Park <thisis_tw94@naver.com>
2023-07-20 21:09:36 +0900

    cloudinary 이용 - upload

--- src/api/uploader.js
+++ src/api/uploader.js
@@ -2,4 +2,9 @@ export async function uploadImage(file) {
   const data = new FormData();
   data.append('file', file);
   data.append('upload_preset', 'process.env.REACT_APP_CLOUDINARY_PRESET');
+
+  return fetch(process.env.REACT_APP_CLOUDINARY_URL, {
+    method: 'POST',
+    body: data,
+  });
 }
