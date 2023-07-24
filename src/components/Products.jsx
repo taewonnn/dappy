d18ed0df983ead58bf302eb5ffd3175b3c85d663
TaeWon Park <thisis_tw94@naver.com>
2023-07-24 20:32:10 +0900

    products 불러오기

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,7 +1,19 @@
+import { useQuery } from "@tanstack/react-query";
+import { getProducts } from "../api/firebase";
+
 export default function Products() {
+
+  const { isLoading, error, data: products } = useQuery(['products'], getProducts);
+
   return (
     <>
-      Products
+      { isLoading && <p>Loading...</p> }
+      { error && <p>{error}</p> }
+      <ul>
+        {products && products.map((product) => (
+          <ProductCard key={product.id} product={product} />
+          ))}
+      </ul>
     </>
   )
 }
