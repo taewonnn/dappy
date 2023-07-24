1ca16a6dee412b09c282c82454d398696e7f8700
TaeWon Park <thisis_tw94@naver.com>
2023-07-24 20:39:09 +0900

    Product 보여주기

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,7 +1,12 @@
 export default function ProductCard({ product: { id, image, title, category, price } }) {
   return (
     <li>
-      {title}
+      <img src={image} alt={title} />
+      <div>
+        <h3>{title}</h3>
+        <p>{`₩${price}`}</p>
+      </div>
+      <p>{category}</p>
     </li>
   )
 }
