31234ca259d8e92c180cd8495cfdca1096a3c321
TaeWon Park <thisis_tw94@naver.com>
2023-07-25 21:10:55 +0900

    Banner

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,10 @@
 import Products from "../components/Products";
+import Banner from "../components/Banner";
 
 export default function Home() {
   return (
     <>
+      <Banner />
       <Products />
     </>
   )
