fa7c0427ec487b25ff7f6359e33b15b5bc780a01
TaeWon Park <thisis_tw94@naver.com>
2023-07-25 21:16:22 +0900

    Banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,8 +1,8 @@
 export default function Banner() {
   return (
-    <section>
+    <section className='h-96 bg-yellow-100 relative'>
       <div></div>
-      <div>
+      <div className='absolute w-full top-32 text-center text-black-50 drop-shadow-2xl'>
         <h2>Shop With us</h2>
         <p>Best Products, High Quality</p>
       </div>
