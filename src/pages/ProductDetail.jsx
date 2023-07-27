934664494cf379fb93eb00518c8c08b00fae3ae7
TaeWon Park <thisis_tw94@naver.com>
2023-07-27 21:48:39 +0900

    detail page

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,7 +1,16 @@
+import { useLocation } from "react-router-dom";
+
 export default function ProductDetail() {
+
+  const {
+    state: {
+      product: { id, image, title, description, category, price, options }
+    }
+  } = useLocation();
+
   return (
-    <>
-      productDetail
-    </>
+    <section>
+      <p></p>
+    </section>
   )
 }
