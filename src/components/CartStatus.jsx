1c7c69a2f03455e308534753ede9be2ea99fad24
TaeWon Park <thisis_tw94@naver.com>
2023-07-30 21:26:32 +0900

    장바구니 개수 - 뱃지

--- src/components/CartStatus.jsx
+++ src/components/CartStatus.jsx
@@ -1,9 +1,11 @@
 import { AiOutlineShoppingCart } from "react-icons/ai";
 import { useQuery } from "@tanstack/react-query";
 import { getCart } from "../api/firebase";
+import { useAuthContext } from "../context/AuthContext";
 
 export default function CartStatus() {
 
+  const { uid } = useAuthContext();
   const { data: products } = useQuery(['carts'], getCart);
 
   return (
