f4adfb0f60840baf2a331187a82a2b2c6584b949
TaeWon Park <thisis_tw94@naver.com>
2023-07-31 21:40:36 +0900

    장바구니 페이지

--- src/pages/MyCart.jsx
+++ src/pages/MyCart.jsx
@@ -1,4 +1,16 @@
+import { useAuthContext } from "../context/AuthContext";
+import { useQuery } from "@tanstack/react-query";
+import { getCart } from "../api/firebase";
+
 export default function MyCart() {
+
+  const { uid } = useAuthContext();
+  const { isLoading, data: products } = useQuery(['carts'], () => getCart(uid));
+
+  if(isLoading) return <p>Loading...</p>;
+
+  const hasProducts = products && products.length > 0;
+
   return (
     <>
       MyCart
