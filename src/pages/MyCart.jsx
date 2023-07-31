78a64fac74eb17e9f618317ea9b38b1613fb1b3a
TaeWon Park <thisis_tw94@naver.com>
2023-07-31 21:48:34 +0900

    장바구니 페이지

--- src/pages/MyCart.jsx
+++ src/pages/MyCart.jsx
@@ -1,13 +1,14 @@
 import { useAuthContext } from "../context/AuthContext";
 import { useQuery } from "@tanstack/react-query";
 import { getCart } from "../api/firebase";
+import CartItem from "../components/Cartitem";
 
 export default function MyCart() {
 
-  const { uid } = useAuthContext();
-  const { isLoading, data: products } = useQuery(['carts'], () => getCart(uid));
+  const {uid} = useAuthContext();
+  const {isLoading, data: products} = useQuery(['carts'], () => getCart(uid));
 
-  if(isLoading) return <p>Loading...</p>;
+  if (isLoading) return <p>Loading...</p>;
 
   const hasProducts = products && products.length > 0;
 
@@ -15,11 +16,20 @@ export default function MyCart() {
     <section>
       <p>나의 장바구니</p>
       {hasProducts && <p>장바구니에 상품이 없습니다!!</p>}
-      {hasProducts && <>
-        <ul>
-          {products && products.map((product) => <CartItem />)
-        </ul>
-      </>}
-    </section>
-  )
+      {hasProducts && (
+        <>
+          <ul>
+            { products &&
+              products.map((product)) => (
+                <CartItem key={product.id} product={product} />
+              ))}
+          </ul>
+          <div>
+
+          </div>
+        </>
+      )}
+      </section>
+  );
 }
+
