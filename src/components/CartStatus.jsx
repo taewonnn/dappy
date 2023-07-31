e6aff57893c5ac6ad5c03d127b78a921830a2294
TaeWon Park <thisis_tw94@naver.com>
2023-07-31 21:37:10 +0900

    장바구니 개수 - 뱃지

--- src/components/CartStatus.jsx
+++ src/components/CartStatus.jsx
@@ -9,9 +9,9 @@ export default function CartStatus() {
   const { data: products } = useQuery(['carts'], () => getCart(uid));
 
   return (
-    <>
-      <AiOutlineShoppingCart />
-      { products && <p>{products.length}</p> }
-    </>
+    <div className='relative'>
+      <AiOutlineShoppingCart className='text-4xl'/>
+      { products && <p className='w-6 h-6 text-center bg-brand text-white font-bold rounded-full absolute -top-1 -right-2'>{products.length}</p> }
+    </div>
   )
 }
