743ac80455e69003ae0a0efef1ad29e5cf70872f
TaeWon Park <thisis_tw94@naver.com>
2023-08-01 20:53:03 +0900

    장바구니 페이지

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,19 +1,19 @@
 import { AiOutlineMinusSquare, AiOutlinePlusSquare } from "react-icons/ai";
 import { RiDeleteBin5Fill } from "react-icons/ri";
+import { addOrUpdateToCart } from "../api/firebase";
 
-export default function CartItem({ product, product: {id, image, title, option, quantity, price} }) {
+export default function CartItem({ product, product: {id, image, title, option, quantity, price}, uid }) {
 
   const handleMinus = (e) => {
-
+    if(quantity < 2) return;
+    addOrUpdateToCart(uid, { ...product, quantity: quantity - 1 })
   }
 
-  const handlePlus = (e) => {
+  const handlePlus = (e) => addOrUpdateToCart(uid, { ...product, quantity: quantity + 1 })
 
-  }
 
-  const handleDelete = (e) => {
+  const handleDelete = () => addOrUpdateToCart(uid, id);
 
-  }
 
   return (
     <li>
