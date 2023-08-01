5c58a918d6227da752ed9acb3e6c5d430b0803af
TaeWon Park <thisis_tw94@naver.com>
2023-08-01 20:48:33 +0900

    장바구니 페이지

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,7 +1,20 @@
+import { AiOutlineMinusSquare, AiOutlinePlusSquare } from "react-icons/ai";
+import { RiDeleteBin5Fill } from "react-icons/ri";
+
 export default function CartItem({ product, product: {id, image, title, option, quantity, price} }) {
   return (
     <li>
-      <img src={image} alt='dd'/>
+      <img src={image} alt={title} />
+      <div>
+        <p>{title}</p>
+        <p>{option}</p>
+        <div>
+          <AiOutlineMinusSquare />
+          <span>{quantity}</span>
+          <AiOutlinePlusSquare />
+          <RiDeleteBin5Fill />
+        </div>
+      </div>
     </li>
   )
 }
