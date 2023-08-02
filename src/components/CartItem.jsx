c4689b905f9eee63af22df56987f0ef8bc99a483
TaeWon Park <thisis_tw94@naver.com>
2023-08-02 21:59:28 +0900

    장바구니 페이지 - ui

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -17,14 +17,14 @@ export default function CartItem({ product, product: {id, image, title, option, 
   return (
     <li className='flex justify-between my-2 items-center'>
       <img className='w-24 md:w-48 rounded-lg' src={image} alt={title} />
-      <div className='flex-1 flex justify-between'>
+      <div className='flex-1 flex justify-between ml-4'>
         <div className='basis-3/5'>
           <p className='text-lg'>{title}</p>
           <p className='text-xl font-bold text-brand'>{option}</p>
           <p>{price}</p>
         </div>
-        <div>
-          <AiOutlineMinusSquare onClick={handleMinus}/>
+        <div className='text-2xl flex items-center'>
+          <AiOutlineMinusSquare className='transition-all cursor-pointer  hover:text-brand hover:scale-105 mx-1' onClick={handleMinus}/>
           <span>{quantity}</span>
           <AiOutlinePlusSquare onClick={handlePlus} />
           <RiDeleteBin5Fill onClick={handleDelete} />
