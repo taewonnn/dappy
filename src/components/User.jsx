1726834579eb241f76d935ee7defa1e08bc4d4fa
TaeWon Park <thisis_tw94@naver.com>
2023-08-22 21:44:04 +0900

    feat: function User

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,8 +1,12 @@
-export default function ({user: {photoURL, displayName}}) {
+export default function User({ user: { photoURL, displayName } }) {
   return (
-    <div className='flex items-center shrink-0'>
-      <img className='w-10 h-10 rounded-full mr-2' src={photoURL} alt={displayName}/>
-      <span className='hidden md:block '>{displayName}</span>
+    <div className="flex items-center shrink-0">
+      <img
+        className="w-10 h-10 rounded-full mr-2"
+        src={photoURL}
+        alt={displayName}
+      />
+      <span className="hidden md:block ">{displayName}</span>
     </div>
-  )
+  );
 }
