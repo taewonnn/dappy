714e2493ba11e082ea21d3551dcb9a3675096180
TaeWon Park <thisis_tw94@naver.com>
2023-08-26 14:15:57 +0900

    chore: 주석

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -3,18 +3,25 @@ import { getProducts } from "../api/firebase";
 import ProductCard from "./ProductCard";
 
 export default function Products() {
-
-  const { isLoading, error, data: products } = useQuery(['products'], getProducts);
+  const {
+    isLoading,
+    error,
+    data: products,
+  } = useQuery(["products"], getProducts);
 
   return (
     <>
-      { isLoading && <p>Loading...</p> }
-      { error && <p>{error}</p> }
-      <ul className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4'>
-        {products && products.map((product) => (
-          <ProductCard key={product.id} product={product} />
+      {/* 로딩 중 */}
+      {isLoading && <p>Loading...</p>}
+      {/* 에러 */}
+      {error && <p>{error}</p>}
+      {/* products map */}
+      <ul className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
+        {products &&
+          products.map((product) => (
+            <ProductCard key={product.id} product={product} />
           ))}
       </ul>
     </>
-  )
+  );
 }
